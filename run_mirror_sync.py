0e46420b1f85a3e382dfeab45723568440f22097
Lewis <107374486+Lewis0770@users.noreply.github.com>
2025-07-06 22:47:59 -0400

    Update run_mirror_sync.py

--- run_mirror_sync.py
+++ run_mirror_sync.py
@@ -1,5 +1,10 @@
-# run_mirror_sync.py
-from modules import mirror_priority_cards
+#!/usr/bin/env python3
+"""
+Entry point for the Trello card mirroring script.
+This file is called by GitHub Actions.
+"""
+
+import mirror_priority_cards
 
 if __name__ == "__main__":
     mirror_priority_cards.main()
